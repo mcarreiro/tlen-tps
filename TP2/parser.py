34d4576f3f79ff82db17248938e45100e3d35134
martin301290@gmail.com <martin301290@gmail.com@21dc6295-3604-c8cf-f585-2e52db89ae01>
2013-12-08 22:32:24 +0000

    asd

--- TP2/parser.py
+++ TP2/parser.py
@@ -6,28 +6,62 @@
 # -----------------------------------------------------------------------------
 
 tokens = (
-    'NAME','NUMBER',
-    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-    'LPAREN','RPAREN',
+    'CON','MIX','ADD','SUB','MUL','DIV','SIN',
+    'LIN','NOI','SIL','PLAY','POST','LOOP','SEMICOLON',
+	'TUNE','FILL','REDUCE','EXPAND','NUMBER','LKEY','RKEY','AMPERSAND',
+	'LPAREN','RPAREN','POINT','COMA', 'FLOAT', 'SIGN', 'PAREN','POSITIVE','NEGATIVE'
     )
 
 # Tokens
 
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_EQUALS  = r'='
+t_CON    = r'con'
+t_MIX   = r'mix'
+t_ADD   = r'add|\+'
+t_SUB  = r'sub|-'
+t_MUL  = r'mul|\*'
+t_DIV  = r'div|/'
+t_SIN  = r'sin'
+t_POSITIVE = r'\+'
+t_NEGATIVE = r'-'
+
+t_LIN  = r'lin|linear'
+t_NOI  = r'noi|noise'
+t_SIL  = r'sil|silence'
+t_PLAY  = r'play'
+t_POST  = r'post'
+t_LOOP  = r'loop'
+t_TUNE  = r'tune'
+t_FILL  = r'fill'
+t_REDUCE  = r'reduce'
+t_EXPAND  = r'expand'
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
-t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_LKEY  = r'{'
+t_RKEY  = r'}'
+t_POINT  = r'.'
+t_COMA = r','
+t_SEMICOLON = r';'
+t_AMPERSAND = r'&'
+
+#t_SIGN = r'(\+|-)?'
+#t_PAREN = r'(\(\))?'
+#t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+def t_FLOAT(t):
+    r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print("Float value too large %d", t.value)
+        t.value = 0
+    return t
 
 def t_NUMBER(t):
     r'\d+'
     try:
-        t.value = int(t.value)
+        t.value = float(t.value)
     except ValueError:
-        print("Integer value too large %d", t.value)
+        print("Float value too large %d", t.value)
         t.value = 0
     return t
 
@@ -48,52 +82,67 @@ lex.lex()
 
 # Parsing rules
 
-precedence = (
-    ('left','PLUS','MINUS'),
-    ('left','TIMES','DIVIDE'),
-    ('right','UMINUS'),
-    )
+#precedence = (
+#    ('left','PLUS','MINUS'),
+#    ('left','TIMES','DIVIDE'),
+#    ('right','UMINUS'),
+#    )
 
 # dictionary of names
-names = { }
+#names = { }
 
-def p_statement_assign(t):
-    'statement : NAME EQUALS expression'
-    names[t[1]] = t[3]
+def p_s(t):
+    '''s : LKEY s RKEY a
+				 | g b'''
+    #names[t[1]] = t[3]
 
-def p_statement_expr(t):
-    'statement : expression'
-    print(t[1])
+def p_a(t):
+    '''a : POINT p a 
+		 | b'''
+    #print(t[1])
 
-def p_expression_binop(t):
-    '''expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression TIMES expression
-                  | expression DIVIDE expression'''
-    if t[2] == '+'  : t[0] = t[1] + t[3]
-    elif t[2] == '-': t[0] = t[1] - t[3]
-    elif t[2] == '*': t[0] = t[1] * t[3]
-    elif t[2] == '/': t[0] = t[1] / t[3]
+def p_b(t):
+	'''b : o s
+		 | '''
 
-def p_expression_uminus(t):
-    'expression : MINUS expression %prec UMINUS'
-    t[0] = -t[2]
+def p_g(t):
+	'''g : SIN LPAREN NUMBER COMA FLOAT RPAREN
+		 | LIN LPAREN FLOAT COMA FLOAT RPAREN
+		 | SIL paren
+		 | NOI LPAREN FLOAT RPAREN
+		 | FLOAT
+		 | SIN LPAREN NUMBER RPAREN'''
 
-def p_expression_group(t):
-    'expression : LPAREN expression RPAREN'
-    t[0] = t[2]
+#	 | NOISE
 
-def p_expression_number(t):
-    'expression : NUMBER'
-    t[0] = t[1]
+def p_o(t):
+	'''o : SEMICOLON
+		| CON
+		| AMPERSAND
+		| MIX
+	 	| ADD
+		| SUB
+		| MUL
+		| DIV'''
+	
 
-def p_expression_name(t):
-    'expression : NAME'
-    try:
-        t[0] = names[t[1]]
-    except LookupError:
-        print("Undefined name '%s'" % t[1])
-        t[0] = 0
+def p_p(t):
+	'''p : PLAY LPAREN FLOAT RPAREN
+		 | POST PAREN
+		 | LOOP LPAREN NUMBER RPAREN
+		 | TUNE LPAREN sign NUMBER RPAREN
+		 | FILL LPAREN FLOAT RPAREN
+		 | REDUCE paren
+		 | EXPAND paren'''
+
+def p_sign(t):
+	''' sign : POSITIVE
+			 | NEGATIVE
+			 | '''
+
+def p_paren(t):
+	''' paren : LPAREN RPAREN
+			 | '''
 
 def p_error(t):
     print("Syntax error at '%s'" % t.value)
