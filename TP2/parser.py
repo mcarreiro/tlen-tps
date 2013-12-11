c94b07f3f8785e9328b6c6e20f2ed3fc989f3f6c
martin301290@gmail.com <martin301290@gmail.com@21dc6295-3604-c8cf-f585-2e52db89ae01>
2013-12-11 18:35:45 +0000

    metodos y parser casi completo

--- TP2/parser.py
+++ TP2/parser.py
@@ -5,18 +5,28 @@ def p_s(t):
 	'''s : LKEY s RKEY a
 		 | g b'''
 
-	if len(t) == 3:		
-		if t[1] != None and t[2] != None:
+	if len(t) == 3: #Caso G B		
+		if t[1] != None and t[2] != None: #Caso Generador y Resto Operador
 			t[0] = oper(t[2]['operator'],t[1],t[2]['value'])
-		elif t[1] != None and t[2] == None:
+		elif t[1] != None and t[2] == None: #Caso Generador y NO Operador
 			t[0] = t[1]
 		else:
-			pass
-	else:
-		pass
+			pass #Aca no deberia levantar Exception?
+	else: #Caso {S}A
+		a = t[4]
+		res = t[2]
+		if res != None:
+			while a != None
+				if len(a) == 3: #Caso methodo
+					res = method(a['method'],res,a['arg1'])
+					a = a['rest']
+				elif len(a) == 2: #Caso operador
+					res = oper(a['operator'],res,a['value'])
+					a = None
+				else: #Caso Vacio
+					a = None
 
-	print t[0]
-	
+			t[0] = res
 
 def p_a(t):
 	'''a : POINT p a 
@@ -24,7 +34,7 @@ def p_a(t):
 	if t[1] != '.':
 		t[0] = t[1]
 	else:
-		obj = {'method': t[2], value: t[3]}
+		obj = {'method': t[2]['method'],'arg1':t[2]['arg1'], 'rest': t[3]}
 		t[0] = obj
 
 def p_b(t):
@@ -64,12 +74,21 @@ def p_o(t):
 
 def p_p(t):
 	'''p : PLAY LPAREN FLOAT RPAREN
-		 | POST PAREN
+		 | POST paren
 		 | LOOP LPAREN NUMBER RPAREN
 		 | TUNE LPAREN sign NUMBER RPAREN
 		 | FILL LPAREN FLOAT RPAREN
 		 | REDUCE paren
 		 | EXPAND paren'''
+	if len(t) == 3: #Casos POST, REDUCE y EXPAND
+		obj = {'method': t[1], 'arg1':None}
+		t[0] = obj
+	elif len(t) == 5: #Casos PLAY, LOOP, FILL
+		obj = {'method': t[1], 'arg1':t[3]}
+		t[0] = obj
+	else: #Caso TUNE
+		obj = {'method': t[1], 'arg1':t[3]*t[4]}
+		t[0] = obj
 
 def p_sign(t):
 	'''sign : POSITIVE
@@ -84,6 +103,13 @@ def p_sign(t):
 def p_paren(t):
 	'''paren : LPAREN RPAREN
 			 | '''
+
+	#Si REDUCE Y EXPAND, no reciben parámetros, entonces 
+
+	# if t[1]:
+	# 	t[0] = t[2]
+	# else:
+	# 	t[0] = 1
 
 def p_error(t):
 	print("Syntax error at '%s'" % t.value)
