ffd2cbc974db3b0077945885891a1997262dd5f7
martin301290@gmail.com <martin301290@gmail.com@21dc6295-3604-c8cf-f585-2e52db89ae01>
2013-12-12 15:12:03 +0000

    lala

--- TP2/main.py
+++ TP2/main.py
@@ -6,7 +6,7 @@ yacc.yacc()
 while 1:
     try:
         # s = input('calc > ')   # Use raw_input on Python 2
-        s = '{3;5.loop(2)}.post'
+        s = '{{-1;1}.loop(44).expand(1).tune(-1).loop(3).fill(3)}.loop(5).post()'
     except EOFError:
         break
     yacc.parse(s)
