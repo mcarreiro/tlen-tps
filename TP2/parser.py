d31b0ad1dd7e7f699022ec0d3190a5e052052bd3
martin301290@gmail.com <martin301290@gmail.com@21dc6295-3604-c8cf-f585-2e52db89ae01>
2013-12-16 01:34:13 +0000

    lasdkl

--- TP2/parser.py
+++ TP2/parser.py
@@ -3,7 +3,7 @@ from funciones import *
 
 precedence = (
     ('right','UMINUS'),
-    ('left','CON'),
+    ('right','SUB','CON'),
     )
 
 def p_s1(t):
@@ -13,24 +13,12 @@ def p_s2(t):
 	'''s : s POINT p'''
 	t[0] = method(t[3]['method'],t[1],t[3]['arg1'])
 def p_s3(t):
-	'''s : s o s %prec CON'''
-	print t[1]
-	print t[2]
-	print t[3]
+	'''s : s o s '''
 	t[0] = oper(t[2],t[1],t[3])
-	print '='
-	print t[0]
-	print '\n'
 
-def p_s5(t):
-	'''s : s CON s'''
-	print t[1]
-	print t[2]
-	print t[3]
-	t[0] = oper(t[2],t[1],t[3])
-	print '='
-	print t[0]
-	print '\n'
+# def p_s5(t):
+# 	'''s : s CON s'''
+# 	t[0] = oper(t[2],t[1],t[3])
 
 def p_s4(t):
 	'''s : LKEY s RKEY'''
@@ -133,7 +121,8 @@ def p_g(t):
 #     t[0] = calcularGA(t[2],t[3])
 
 def p_o(t):
-	'''o : MIX
+	'''o : CON 
+		| MIX
 		| ADD
 		| SUB
 		| MUL
@@ -144,26 +133,34 @@ def p_p(t):
 	'''p : PLAY LPAREN FLOAT RPAREN
 		 | POST paren
 		 | LOOP LPAREN FLOAT RPAREN
-		 | TUNE LPAREN FLOAT RPAREN
+		 | TUNE LPAREN sign FLOAT RPAREN
 		 | FILL LPAREN FLOAT RPAREN
 		 | REDUCE LPAREN FLOAT RPAREN
 		 | EXPAND paramopcional'''
 	if len(t) == 3: #Casos POST y EXPAND
 		obj = {'method': t[1], 'arg1':t[2]}
 		t[0] = obj
-	else: #Casos PLAY, LOOP, TUNE, FILL, REDUCE
+	elif len(t) == 6: #CASO TUNE
+		obj = {'method': t[1], 'arg1':t[3]*t[4]}
+		t[0] = obj
+	else: #Casos PLAY, LOOP, FILL, REDUCE
 		obj = {'method': t[1], 'arg1':t[3]}
 		t[0] = obj
 
-# def p_sign(t):
-# 	'''sign : POSITIVE
-# 			| NEGATIVE
-# 			| '''
-# 	if len(t) == 2 and t[1] == '-':
-# 		print t[1]
-# 		t[0] = -1
-# 	else:
-# 		t[0] = 1
+def p_sign(t):
+	'''sign : ADD
+			| SUB
+			| '''
+	if len(t) == 2 and t[1] == '-':
+		t[0] = -1
+	elif len(t) == 2 and t[1] == 'sub':
+		print 'Error, sub no puede usarse para indicar un numero negativo'
+		exit()
+	elif len(t) == 2 and t[1] == 'add':
+		print 'Error, add no puede usarse para indicar un numero positivo'
+		exit()
+	else:
+		t[0] = 1
 
 
 def p_paren(t):
